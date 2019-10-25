2fb65adc83e4edeec8000c0659397342e508ab33
Alex Lindman <alexandral@unity3d.com>
2019-10-25 10:00:37 -0700

    sanitize transform to tangent string

--- com.unity.shadergraph/Editor/Data/Nodes/Math/Vector/TransformNode.cs
+++ com.unity.shadergraph/Editor/Data/Nodes/Math/Vector/TransformNode.cs
@@ -100,7 +100,7 @@ namespace UnityEditor.ShaderGraph
         {
             NodeUtils.SlotConfigurationExceptionIfBadConfiguration(this, new[] { InputSlotId }, new[] { OutputSlotId });
             string inputValue = string.Format("{0}.xyz", GetSlotValue(InputSlotId, generationMode));
-            string targetTransformString = "tangentTransform_" + conversion.from.ToString();
+            string targetTransformString = GetVariableNameForNode() + "_tangentTransform_" + conversion.from.ToString();
             string transposeTargetTransformString = GetVariableNameForNode() + "_transposeTangent";
             string transformString = "";
             string tangentTransformSpace = conversion.from.ToString();
